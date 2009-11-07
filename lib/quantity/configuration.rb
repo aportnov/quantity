b6ab8d08f0bc8abd0174df8c5e88868bfd902244
Alex Portnov <alex.portnov@gmail.com>
2009-11-07 16:39:46 -0600

    starting refactoring. Removed global configuration variable. It is scoped to Quantity now

--- lib/quantity/configuration.rb
+++ lib/quantity/configuration.rb
@@ -1,31 +1,31 @@
-class Configuration
-  
-  def initialize
-    @default = Quantity::Calculator.new
-    yield self if block_given?
-  end
-  
-  def unit (params)
-    @default.add_unit params
-  end
-  
-  def conversion (params)
-    @default.add_conversion params
-  end
-  
-  def calc
-    calc = Quantity::Calculator.new({}.replace(@default.units))
-    
-    @default.conversions.each {|c| 
-      calc.conversions << Quantity::Quantity.new(calc, :unit => c.unit, :value => c.value)
-    }
-    
-    return calc    
-  end
-  
-end
-
 module Quantity
+  class Configuration
+  
+    def initialize
+      @default = Calculator.new
+      yield self if block_given?
+    end
+  
+    def unit (params)
+      @default.add_unit params
+    end
+  
+    def conversion (params)
+      @default.add_conversion params
+    end
+  
+    def calc
+      calc = Calculator.new({}.replace(@default.units))
+    
+      @default.conversions.each {|c| 
+        calc.conversions << Quantity.new(calc, :unit => c.unit, :value => c.value)
+      }
+    
+      return calc    
+    end
+  
+  end
+
   def self.default(&block)
     @config ||= Configuration.new()
     yield @config if block_given? 
